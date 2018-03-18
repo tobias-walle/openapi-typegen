53485d69ac823dc69dce9b5c5ba5722b76e0ad52
Tobias Walle <tobias.walle@maibornwolff.de>
2018-03-18 21:24:19 +0100

    feat: Add a more complicated example

--- examples/pet-store/definitions.ts
+++ examples/pet-store/definitions.ts
@@ -1,12 +1,44 @@
-export interface Pet {
-    id: number;
-    name: string;
-    tag?: string;
+export interface Order {
+    id?: number;
+    petId?: number;
+    quantity?: number;
+    shipDate?: string;
+    status?: string;
+    complete?: boolean;
 }
 
-export type Pets = Pet[];
+export interface Category {
+    id?: number;
+    name?: string;
+}
 
-export interface Error {
-    code: number;
-    message: string;
+export interface User {
+    id?: number;
+    username?: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    password?: string;
+    phone?: string;
+    userStatus?: number;
+}
+
+export interface Tag {
+    id?: number;
+    name?: string;
+}
+
+export interface Pet {
+    id?: number;
+    category?: Category;
+    name: string;
+    photoUrls: string;
+    tags?: string;
+    status?: string;
+}
+
+export interface ApiResponse {
+    code?: number;
+    type?: string;
+    message?: string;
 }
